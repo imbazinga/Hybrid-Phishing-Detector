5994e8701a5be0300ed287cdc9329071d12fca48
salman ahmed <127968560+imbazinga@users.noreply.github.com>
2025-10-19 13:51:32 +0530

    Update machinelearning.py

--- backend/machinelearning.py
+++ backend/machinelearning.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, Gradien
 from sklearn.metrics import accuracy_score, classification_report
 import joblib
 # Load the phishing dataset
-df = pd.read_csv("C:\\Users\\karki\\Desktop\\complete MAIN\\phishing_site_urls.csv", encoding='latin1')
+df = pd.read_csv("phishing_site_urls.csv", encoding='latin1')
 # Split the dataset into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(df.drop('URL', axis=1), df['Label'], test_size=0.2, random_state=42)
 # Create an ensemble model
@@ -18,4 +18,5 @@ accuracy = accuracy_score(y_test, y_pred)
 print('Accuracy:', accuracy)   
 print(classification_report(y_test, y_pred))
 # Save the trained model
+
 joblib.dump(model,'phishing_model.pkl')
